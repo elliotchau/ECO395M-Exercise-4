7a9ecbe411c7b39d6290a79f4f30420528bf83f9
Elliot Chau <elliot.chau@utexas.edu>
2019-04-27 15:38:21 -0500

    commit

--- Problem 1/wine.R
+++ Problem 1/wine.R
@@ -12,24 +12,24 @@ X = wine[,c(1:11)]
 pc1 = prcomp(X, scale=TRUE, center=TRUE)
 loadings = pc1$rotation
 scores = pc1$x
-summary(pc1)
-autoplot(pc1)
+summary(pc1) # statisitics about each component
+biplot(pc1) # each wine's ID with chemical characteristics vectors
+autoplot(pc1) # a closer look with just the data points
 # revealing the colors associated for each group (checking work)
-scores = pc1$x
 qplot(scores[,1], scores[,2], color=wine$color, xlab='Component 1', ylab='Component 2')
 # seeing how PCA performed
-xtabs(~cluster1$cluster + wine$color)
+# PCAcluster = kmeans(scores[,1:4], 2, nstart=25)
+# xtabs(~PCAcluster$cluster + wine$color)
 
 # correlation matrix for next part
 rquery.cormat(X)
 # this allows us to identify correlations between chemicals
 
 # k means clustering for wine color
-X = wine[,c(1:11)]
 X = scale(X, center=TRUE, scale=TRUE)
 mu = attr(X,"scaled:center")
 sigma = attr(X,"scaled:scale")
-# run k-means with 2 clusters -- one for white and red wine
+# run k-means with 2 clusters for each color
 cluster1 = kmeans(X, 2, nstart=25)
 # examining statistics in the 2 clusters
 cluster1$center[1,]*sigma + mu
@@ -39,10 +39,12 @@ qplot(volatile.acidity, alcohol, data=wine, color=factor(cluster1$cluster))
 qplot(residual.sugar, pH, data=wine, color=factor(cluster1$cluster))
 qplot(fixed.acidity, chlorides, data=wine, color=factor(cluster1$cluster))
 qplot(total.sulfur.dioxide, density, data=wine, color=factor(cluster1$cluster))
+qplot(alcohol, density, data=wine, color=factor(cluster1$cluster))
+
+# k means performance using supervised info
+xtabs(~cluster1$cluster + wine$color)
       
 # 1 is red, 2 is white
-# checking work
-# qplot(volatile.acidity, alcohol, data=wine, color=wine$color)
 ################################################################################
 
 # k-means for wine quality
@@ -53,12 +55,13 @@ cluster2 = kmeans(X, 7, nstart=25)
 qplot(volatile.acidity, alcohol, data=wine, color=factor(cluster2$cluster))
 qplot(residual.sugar, pH, data=wine, color=factor(cluster2$cluster))
 qplot(fixed.acidity, chlorides, data=wine, color=factor(cluster2$cluster))
-qplot(total.sulfur.dioxide, density, data=wine, color=factor(cluster2$cluster))
+qplot(total.sulfur.dioxide, density, data=wine, color=factor(cluster2$cluster)) # interesting
+# evidence against quality prediction for k-means
+xtabs(~cluster2$cluster + wine$quality)
 
 # principal component analysis for wine quality
-# just as useless
 pc2 = kmeans(scores[,1:11], 7, nstart=25)
+# clustered into 7 groups
 qplot(scores[,1], scores[,2], color=factor(pc2$cluster), xlab='Component 1', ylab='Component 2')
-
-# evidence that quality prediction is no good
-xtabs(~cluster2$cluster + wine$quality)
+# indicates that it was unable to cluster by quality (checking work)
+qplot(scores[,1], scores[,2], color=factor(wine$quality), xlab='Component 1', ylab='Component 2')
---
+++ Problem 2/Data Mining HW 4, Q2.R
@@ -0,0 +1,104 @@
+library(skimr)
+str(social_marketing)
+
+skim(social_marketing)
+
+summary(social_marketing)
+
+cor(social_marketing[,2:37])
+
+library(GGally)
+ggcorr(social_marketing[,2:37])
+
+table(social_marketing$spam, social_marketing$adult)
+table(social_marketing$adult)
+prop.table(table(social_marketing$spam, social_marketing$adult))
+
+# Dropping all observations marked with spam
+SM_spam_free <- social_marketing[!(social_marketing$spam > "0"),]
+
+# Checking to see if all observations with spam were dropped
+table(SM_spam_free$spam)
+
+# Dropping the spam variable
+SM <- SM_spam_free[-c(36)]
+
+# Number of users and the amount of adult content they posted
+table(SM$adult)
+prop.table(table(SM$adult))
+
+sum(SM$adult)
+
+# Find row summery of total tweet count for each user
+rs <- rowSums(SM[,-1])
+
+# Drop users with who have more than 25% of total tweets related to adult content
+rs_2 <- SM[SM$adult/rs<.25,]
+SM <- rs_2
+
+library(dplyr)
+library(tidyr)
+# Check
+SM %>% 
+  group_by(adult, X) %>%
+  # Count occurences of adult flagged tweets per user
+  summarise(n=n()) %>%
+  # Get percentage per user
+  mutate(percent = n / sum(n)*100) %>%
+  filter(percent>20) %>%
+  select(X, adult, percent) %>%
+  arrange(desc(adult))
+
+#----PCA----#
+# Drop users variable X
+SM2 <- SM
+
+# Check distribution for SM2
+SM2 %>% 
+  gather(Variable, Value) %>% 
+  ggplot(aes(x=Value, fill=Variable)) +
+  geom_density(alpha=0.3) +
+  geom_vline(aes(xintercept=0)) +
+  theme_bw()
+
+# PCA
+SM_pca <- prcomp(SM2[,-c(1,10)],
+                 scale. = TRUE,
+                 center = TRUE)
+
+str(SM_pca)
+
+print(SM_pca)
+
+plot(SM_pca, type = 'l') 
+
+SM_pcaDF <- data.frame(SM_pca$x)
+
+plot(SM_pcaDF[,1:5], pch = 16, col=rgb(0,0,0, alpha = .5), cex =.3)
+
+# Summary to find what percent of variation is explained by each PCA 
+summary(SM_pca)
+
+# Plot of PCA
+library(ggplot2)
+library(ggfortify)
+ggplot2::autoplot(SM_pca, loadings = TRUE, label.size = 3, loadings.label = TRUE, loadings.colour = 'red', loadings.label.colour = "blue")
+
+# Find loadings
+loadings = SM_pca$rotation
+
+# these are the 10 tweet groups most negatively associated with Nutrient H20 followers
+loadings[,1] %>% sort %>% head(10)
+
+# these are the 10 tweet groups most positively associated with Nutrient H20 followers
+loadings[,1] %>% sort %>% tail(10)
+
+#----Hierarchical Clustering the PCA's for market segmentation----#
+x = loadings[,1]
+y = loadings[,2:8]
+
+# Cluster and graphing
+# Market segementation using the PCA's 1-8
+hc = hclust(dist(cbind(x,y)), method = 'ward.D')
+plot(hc, axes=F, xlab='Twitter Categories', ylab='Market Segmentation', sub ='', main='Clustering of Principle Components 1-8')
+rect.hclust(hc, k=6, border='red')
